bbb728ff277ecfe35a873794a0a1f8e02ca927c2
Patryk Matuszak <305846+pmtk@users.noreply.github.com>
2022-08-22 18:09:10 +0200

    focus on RTA callgraph based approach

--- main.go
+++ main.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"io/fs"
 	"path/filepath"
+	"strings"
+	"time"
 
-	"github.com/davecgh/go-spew/spew"
+	"golang.org/x/tools/go/callgraph"
+	"golang.org/x/tools/go/callgraph/rta"
+	"golang.org/x/tools/go/packages"
+	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
 	klog "k8s.io/klog/v2"
 )
-
-/*
-TODO:
-- Transform API nodes into specific k8s/ocp API packages
-- Export a summary: Ginkgo nodes + API used
-- Run for whole origin repo
-- Handle: `authorizationv1.GroupVersion.WithResource(tt.resource).GroupResource()`
-- Tests can declare local helpers: https://github.com/openshift/origin/blob/80e4580ea73536c8f9193c749cf5c9e14e70e1ab/test/extended/cli/builds.go#L200
-*/
 
 var ignoreDirs = []string{
 	"test/extended/testdata", "test/extended/testdata/",
@@ -45,21 +43,227 @@ func main() {
 		return nil
 	})
 
-	origin, err := ParseOrigin(originPath, pkgs)
+	klog.V(2).Infof("Pkgs to scan: %#v", pkgs)
+	buildReportUsingRTA(originPath, pkgs)
+}
+
+// call is a function call in context of specific ginkgo node
+type call struct {
+	Test    []string // [Describe-Description, [Context-Description...], It-Description]
+	Call    funcCall
+	Ignored bool // Ignored informs if function call is not API calls, but persisted for debugging purposes
+}
+
+type funcCall struct {
+	Pkg  string
+	Func string
+	Args []string
+}
+
+// type pkgCalls struct {
+// 	Pkg  string
+// 	Func []string
+// }
+
+type report struct {
+	// Ignored
+}
+
+func buildReportUsingRTA(originPath string, pkgs []string) error {
+	cfg := packages.Config{
+		Mode:  packages.LoadAllSyntax,
+		Dir:   originPath,
+		Tests: true,
+	}
+	klog.V(2).Infof("packages.Load - start\n")
+	start := time.Now()
+	initial, err := packages.Load(&cfg, "./test/extended/apiserver/") // TODO: pkgs
 	if err != nil {
-		klog.Fatalf("Failed to build test tree: %#v\n", err)
+		return fmt.Errorf("packages.Load failed: %w", err)
+	}
+	klog.V(2).Infof("packages.Load - end after %s\n", time.Since(start))
+
+	klog.V(2).Infof("ssautil.AllPackages - start\n")
+	start = time.Now()
+	prog, _ := ssautil.AllPackages(initial, ssa.InstantiateGenerics) // ssa.PrintPackages
+	klog.V(2).Infof("ssautil.AllPackages - end after %s\n", time.Since(start))
+
+	klog.V(2).Infof("prog.Build - start\n")
+	start = time.Now()
+	prog.Build()
+	klog.V(2).Infof("prog.Build - end after %s\n", time.Since(start))
+
+	fcm := map[string]*ssa.Function{}
+
+	for _, p := range prog.AllPackages() {
+		if p != nil && strings.Contains(p.Pkg.Path(), "github.com/openshift/origin/test/extended/apiserver") {
+			a := p.Members["init"]
+			a2 := a.(*ssa.Function)
+			bb := a2.Blocks[1] // "init.start"
+
+			for _, instr := range bb.Instrs {
+				if c, ok := instr.(*ssa.Call); ok {
+					f := c.Call.Value.(*ssa.Function)
+					if f.Name() == "Describe" {
+						cnst, ok := c.Call.Args[0].(*ssa.Const)
+						if !ok {
+							return fmt.Errorf("expected c.Call.Args[0] to be type *ssa.Const: %#v", c.Call.Args[0])
+						}
+						if cnst.Type().String() != "string" {
+							return fmt.Errorf("expected cnst.Type() to be string: %#v", cnst.Type())
+						}
+						anonFun := c.Call.Args[1].(*ssa.Function)
+						fcm[strings.ReplaceAll(cnst.Value.String(), "\"", "")] = anonFun
+					}
+				}
+			}
+		}
 	}
 
-	printTree(origin.Tests)
-	printTree(origin.Helpers)
-
-	hs, err := resolveHelperTree(origin.Helpers)
-	if err != nil {
-		klog.Fatalf("Failed to resolve helpers: %#v\n", err)
+	fcs := make([]*ssa.Function, 0, len(fcm))
+	for _, f := range fcm {
+		fcs = append(fcs, f)
 	}
 
-	spew.Dump(hs)
+	klog.V(2).Infof("rta.Analyze - start\n")
+	start = time.Now()
+	rtaAnalysis := rta.Analyze(fcs, true)
+	klog.V(2).Infof("rta.Analyze - end after %s\n", time.Since(start))
 
-	// TODO: Go over origin.Tests and transform Helper (flatten its API calls) and API nodes
-	// TODO: Process "transformed API calls" - this should be close to final report of Tests+API their using
+	// TODO: Parallelize traverseNodes + bufferedChannel? Don't think we'd have great speed up since building RTA is most time consuming
+	callChan := make(chan call)
+	calls := []call{}
+
+	go func() {
+		for c := range callChan {
+			calls = append(calls, c)
+		}
+	}()
+
+	klog.V(2).Infof("Traversing tests - start\n")
+	start = time.Now()
+	for desc, fun := range fcm {
+		node, found := rtaAnalysis.CallGraph.Nodes[fun]
+		if !found {
+			return fmt.Errorf("couldn't find %s (%s) in rtaAnalysis.CallGraph.Nodes[]", fun.Name(), desc)
+		}
+		traverseNodes(rtaAnalysis.CallGraph.Nodes, node, []string{desc}, callChan)
+	}
+	klog.V(2).Infof("Traversing tests - end after %s\n", time.Since(start))
+
+	ignored := map[string]struct{}{}
+	mergedApiUsage := map[string]map[string]struct{}{}
+	for _, c := range calls {
+		if c.Ignored {
+			ignored[c.Call.Pkg] = struct{}{}
+		} else {
+			key := strings.Join(c.Test, " ")
+			if mergedApiUsage[key] == nil {
+				mergedApiUsage[key] = map[string]struct{}{}
+			}
+			mergedApiUsage[key][c.Call.Pkg] = struct{}{}
+		}
+	}
+
+	return nil
+}
+
+func traverseNodes(m map[*ssa.Function]*callgraph.Node, node *callgraph.Node, parentTestTree []string, callChan chan<- call) {
+	for _, edge := range node.Out {
+		callee := edge.Callee
+		calleeFuncName := callee.Func.Name()
+		calleePkg := ""
+		if callee.Func.Pkg != nil {
+			calleePkg = callee.Func.Pkg.Pkg.Path()
+		} else if callee.Func.Signature.Recv() != nil {
+			// if Recv != nil, then it's stored as a first parameter
+			if callee.Func.Params[0].Object().Pkg() != nil {
+				// callee can have a receiver, but not be a part of a Pkg like error
+				calleePkg = callee.Func.Params[0].Object().Pkg().Path()
+			}
+		} else {
+			panic("unknown calleePkg")
+		}
+
+		testTree := make([]string, len(parentTestTree))
+		copy(testTree, parentTestTree)
+
+		var childNode *callgraph.Node
+
+		if calleePkg == "github.com/onsi/ginkgo" {
+			if calleeFuncName == "It" || calleeFuncName == "Context" {
+				call := edge.Site.Value()
+				args := call.Call.Args
+				ginkgoDesc := ""
+
+				// arg0 can be const(string) or call(fmt.Sprintf)
+				if cnst, ok := args[0].(*ssa.Const); ok {
+					// ginkgoDesc = cnst.Value.String()
+					ginkgoDesc = cnst.Value.ExactString()
+				} else if c, ok := args[0].(*ssa.Call); ok {
+					callName := c.Call.Value.Name()
+					if callName == "Sprintf" {
+						ginkgoDesc = c.Call.Args[0].(*ssa.Const).Value.String()
+					} else {
+						panic("unknown args[0] call")
+					}
+				} else {
+					panic("unknown args[0] type")
+				}
+
+				f1, ok := call.Call.Args[1].(*ssa.MakeInterface)
+				if !ok {
+					panic(".")
+				}
+				f2, ok := f1.X.(*ssa.MakeClosure)
+				if !ok {
+					panic(".")
+				}
+				f3, ok := f2.Fn.(*ssa.Function)
+				if !ok {
+					panic(".")
+				}
+
+				var found bool
+				// Context/It("desc", func(){))
+				//              visit ^^^^^^^^ by setting childNode
+				childNode, found = m[f3]
+				if !found {
+					panic(".")
+				}
+				testTree = append(testTree, strings.ReplaceAll(ginkgoDesc, "\"", ""))
+			}
+		} else {
+			if strings.Contains(calleePkg, "github.com/openshift/client-go") ||
+				strings.Contains(calleePkg, "k8s.io/client-go") ||
+				strings.Contains(calleePkg, "k8s.io/apimachinery") ||
+				calleePkg == "github.com/openshift/origin/test/extended/util" {
+				// k8s.io/client-go/dynamic.Create("context.Background()", "new k8s.io/apimachinery/pkg/apis/meta/v1/unstructured.Unstructured (complit)", "*t27", "nil:[]string")
+
+				// get recv
+				c := call{Test: testTree, Call: funcCall{Pkg: calleePkg, Func: calleeFuncName}}
+				for _, arg := range edge.Site.Common().Args {
+					c.Call.Args = append(c.Call.Args, arg.String())
+				}
+				callChan <- c
+				fmt.Printf("%#v\n", c)
+
+			} else if strings.Contains(calleePkg, "k8s.io/kubernetes/test/e2e") ||
+				strings.Contains(calleePkg, "github.com/openshift/origin/test/") {
+				// go into the helper functions
+				childNode = callee
+
+			} else {
+				c := call{Test: testTree, Call: funcCall{Pkg: calleePkg}, Ignored: true}
+				for _, arg := range edge.Site.Common().Args {
+					c.Call.Args = append(c.Call.Args, arg.String())
+				}
+				callChan <- c
+			}
+		}
+
+		if childNode != nil {
+			traverseNodes(m, childNode, testTree, callChan)
+		}
+	}
 }
